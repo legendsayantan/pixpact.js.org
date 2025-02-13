0d5a1753740bc7070dff85fef4e3fffb43ce54b8
legendsayantan <legendsayantan@gmail.com>
2025-02-13 18:14:23 +0000

    Update from https://github.com/legendsayantan/PixPact/commit/c4030a5c8c020a7a105f66dc26755aa96e9faa82

--- README.md
+++
@@ -1,1 +0,0 @@
-# pixpact.js.org
---
+++ precache-manifest.f53bea7ec6f15246834a7f35b694db51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d3a399a06d847d54db278c9a16d769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83395f49047f5c725c4c",
+    "url": "/static/css/main.56dc6bf4.chunk.css"
+  },
+  {
+    "revision": "b0ddfb65725329841f09",
+    "url": "/static/js/2.cf2848de.chunk.js"
+  },
+  {
+    "revision": "effbcd701a16b5581b02",
+    "url": "/static/js/3.8bd7b985.chunk.js"
+  },
+  {
+    "revision": "a51d4b4d5dbd3ad8e607",
+    "url": "/static/js/4.b8c3b99d.chunk.js"
+  },
+  {
+    "revision": "a8ab7a715eeecbe9e753",
+    "url": "/static/js/5.60be463b.chunk.js"
+  },
+  {
+    "revision": "83395f49047f5c725c4c",
+    "url": "/static/js/main.9a7765e1.chunk.js"
+  },
+  {
+    "revision": "a9e5e5b3df3d5aa53c20",
+    "url": "/static/js/runtime~main.2af65342.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
