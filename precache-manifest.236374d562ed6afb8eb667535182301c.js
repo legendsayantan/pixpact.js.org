c8007079cc2f59ef5b69ae4b0d8045579cb4c77e
legendsayantan <legendsayantan@gmail.com>
2025-02-18 08:47:30 +0000

    Update from https://github.com/legendsayantan/PixPact/commit/b3c32d07cbfc3938ef2d5ead0b4ac0fa6da7f631

---
+++ precache-manifest.236374d562ed6afb8eb667535182301c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07163fddfceec1999f58878b7d633958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c66fb7f324199e592ec",
+    "url": "/static/css/main.70259cba.chunk.css"
+  },
+  {
+    "revision": "5df91ac88c75c18d67be",
+    "url": "/static/js/2.30bb5901.chunk.js"
+  },
+  {
+    "revision": "ebe91f155a99e607d90e",
+    "url": "/static/js/3.601f171f.chunk.js"
+  },
+  {
+    "revision": "a51d4b4d5dbd3ad8e607",
+    "url": "/static/js/4.b8c3b99d.chunk.js"
+  },
+  {
+    "revision": "a8ab7a715eeecbe9e753",
+    "url": "/static/js/5.60be463b.chunk.js"
+  },
+  {
+    "revision": "3c66fb7f324199e592ec",
+    "url": "/static/js/main.d7f14478.chunk.js"
+  },
+  {
+    "revision": "29045b05cc6a447039a3",
+    "url": "/static/js/runtime~main.fc0b1786.js"
+  },
+  {
+    "revision": "dcc36dcac80353e2a3098dcb820979df",
+    "url": "/static/media/icon.dcc36dca.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fc53ea49afa99d16671715c895d59b7.js"
+  "/precache-manifest.236374d562ed6afb8eb667535182301c.js"
 );
 
 self.addEventListener('message', (event) => {
